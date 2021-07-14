25c4c0d870ef3c7387252581f5d38adeae257a38
shraddha343 <84217195+shraddha343@users.noreply.github.com>
2021-07-14 21:36:36 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+ char ch;
+ printf("\n ENTER THE CHARACTER ");
+ scanf("%c",&ch);
+ switch(ch)
+ {
+  case'a':
+  printf("\n IT IS A VOWEL");
+  break;
+  case'e':
+  printf("\n IT IS A VOWEL");
+  break;
+  case'i':
+  printf("\n IT IS A VOWEL");
+  break;
+  case'o':
+  printf("\n IT IS A VOWEL");
+  break;
+  case'u':
+  printf("\n IT IS A VOWEL");
+  break;
+  default:
+  printf("\n IT IS A CONSONANT");
+ }
+}
