91a28f66f4d94c7fe761328529e3b9aea275687a
shraddha343 <84217195+shraddha343@users.noreply.github.com>
2021-07-15 21:26:10 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+  int a[2][2],b[2][2],c[2][2],i,j;
+  printf("\n ENTER VALUES FOR MATRIX A:\n");
+  for(i=0;i<2;i++)
+   for(j=0;j<2;j++)
+    scanf("%d",&a[i][j]);
+  printf("\n ENTER VALUES FOR MATRIX B:\n");
+  for(i=0;i<2;i++)
+   for(j=0;j<2;j++)
+     scanf("%d",&b[i][j]);
+  for(i=0;i<2;i++)
+   for(j=0;j<2;j++)
+    c[i][j]=a[i][j]+b[i][j];
+  printf("\n THE VALUES OF MATRIX C ARE:\n");
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+     printf("%5d",c[i][j]);
+     printf("\n");
+  }
+  return 0;
+}
